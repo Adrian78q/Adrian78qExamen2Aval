bc346fb1fb63de2ae1ba9c689945c941a15e22c5
adrian rodriguez <adrian78q@gmail.com>
2023-02-16 09:53:25 +0100

    Script cmedia.sh

--- cmedia.sh
+++ cmedia.sh
@@ -4,10 +4,10 @@ if [ -z "$1" ]; then
   exit 1
 fi
 ciudad="$1"
-consumos=$(grep -E "^${ciudad}\s" consumos.txt | awk '{print $4}')
+consumos=$(grep -E "^$ciudad" consumos.txt | awk '{print $4}')
 if [ -z "$consumos" ]; then
-  echo "No se han encontrado consumos en ${ciudad}."
+  echo "No se han encontrado consumos en $ciudad."
   exit 1
 fi
 media=$(echo "$consumos" | awk '{sum += $1} END {print sum/NR}')
-echo "La media de consumos de la ciudad ${ciudad} es: ${media}"
+echo "La media de consumos de la ciudad $ciudad es: $media"
